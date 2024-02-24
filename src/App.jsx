b90166b8e1ae6f39fe7c9ada218323075a866643
Nahid <allahparastnahid@gmail.com>
2024-02-24 20:24:19 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppNav from "./components/AppNav";
+const App = () => {
+  return (
+    <div>
+      <h1>Single Page Application</h1>
+      <AppNav />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,15 @@
+const AppNav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Posts</li>
+          <li>Dashbord</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default AppNav;
